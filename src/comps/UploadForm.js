1f2942d6d90882486172f6e08c2d20ca4596d2c8
Atul Patil <atulpatil539@gmail.com>
2021-10-17 16:38:05 +0530

    feat: updated

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 
 const UploadFrom = () => {
+    const [file , setFile] = useState(null);
+    const [error, setError] = useState(null);
 
     const changeHandler =(e)=>{
- const [file , setFile] = useState(null);
- const [error, setError] = useState(null);
+
  const types = ['image/png', 'image/jpeg']
 
 
    let selected = e.target.files[0];
   
+  
    if(selected && types.includes(selected.type)){
        setFile(selected)
+       setError(' ')
    }else{
        setFile(null)
-       setError("Plz select an Image file(png or jpeg");
+       setError("Plz select an Image file(png or jpeg)");
 
    }
     }
@@ -22,6 +25,10 @@ const UploadFrom = () => {
     return (
         <form>
             <input type="file" onChange ={changeHandler}/>
+            <div className = "output">
+                {error && <div className = "error">{error}</div>}
+                {file && <div className = "file">{file.name}</div>}
+            </div>
         </form>
     )
 }
---
+++ src/hooks/useStorage.js
@@ -0,0 +1,11 @@
+import { useState , useEffect } from 'react'
+import React from 'react'
+import { projectStorage } from '../firebase/config'
+
+
+
+const useStorage = (file) => {
+    const [progress, setProgress] = useState(0);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState(null);
+}
