e7fffbd9b4f87c73451321187e8eba9d948726c1
Atul Patil <atulpatil539@gmail.com>
2021-10-17 22:23:25 +0530

    feat: updated

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -8,4 +8,9 @@ const useStorage = (file) => {
     const [progress, setProgress] = useState(0);
     const [error, setError] = useState(null);
     const [url, setUrl] = useState(null);
+
+    useEffect(() =>{
+
+    }, [file])
 }
+
